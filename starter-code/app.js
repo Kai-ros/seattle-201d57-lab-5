bce0fbe2e54f8faabbaebd913dcfd7b190cfcb49
Brandon Hurrington <brandonhurrington@yahoo.com>
2019-06-09 08:39:11 -0700

    sumArray test passes.

--- starter-code/app.js
+++ starter-code/app.js
@@ -9,8 +9,6 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-  console.log('a : ' + a);
-  console.log('b : ' + b);
 
   var total = a + b;
   var message = 'The sum of ' + a + ' and ' + b + ' is ' + total + '.';
@@ -92,11 +90,22 @@ var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
 
+  var i = 0;
+  var sumLoop = 0;
+
+  do {
+    sumLoop = sum(sumLoop, sumArr[i++])[0];
+  } while(i < sumArr.length);
+
+  var summedArray = sumLoop;
+  var summedArrayMessage = sumArr[0] + ',' + sumArr[1] + ',' + sumArr[2] + ' was passed in as an array of numbers, and ' + summedArray + ' is their sum.';
+
+  return [summedArray, summedArrayMessage];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
